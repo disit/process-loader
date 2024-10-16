6791d4d51605eba303f3b6783a3d26a00bb00f8e
Distributed Systems and Internet Technology Lab, Distributed Data Intelligence and Tech. Lab <info@disit.org>
2024-10-16 15:14:07 +0200

    updated version

--- www/api/bigdatafordevice/getOneSpecific.php
+++ www/api/bigdatafordevice/getOneSpecific.php
@@ -1,42 +1,116 @@
 <?php
-	header("Access-Control-Allow-Origin: *\r\n");
-	include('../../config.php'); // mi prendo i valori per la connessione: host username password 
-	include('../../curl.php');
-	
-	$suri = $_GET["suri"]; //mi prendo il service uri come parametro dalla dall'uri
-   	$response = []; //inizializzo la response da ritornare quando faccio la chiamata
-   	$myArray = array(); //inizializzo se trovo valori
-	//mi connetto al db
-	$link =  mysqli_connect($host, $username, $password); 
-	mysqli_select_db($link, "processloader_db");
+        header("Access-Control-Allow-Origin: *");
+        include('../../config.php'); // mi prendo i valori per la connessione: host username password
+        include('../../curl.php');
+        require ('../../sso/autoload.php');
+        use Jumbojett\OpenIDConnectClient;
 
-	// se la connessione non è andata a buon fine	
-	if($link == false){
-		$response['result'] = 'KO';
-		$response['code'] = 500;
-		$response['message'] = 'DB Connection error';
-		mysqli_close($link); // chiudo la connessione
-    	echo json_encode($response); //mando il messaggio di errore
-    }else{
-	    //faccio la query
-		$sql = "SELECT * FROM processloader_db.bigdatafordevice where suri= '$suri' order by dateObserved desc ";
-		$result = mysqli_query($link, $sql);
-		if(false === $result) { // se non va a buon fine
-	    	echo "Errore: impossibile eseguire la query. " . mysqli_error($conn);
-	    	$response['result'] = 'KO';
-			$response['code'] = 500;
-			$response['message'] = 'DB query error';
-		}else{ //altrimenti metto tutto in myArray e lo restituisco come json encoded
-			while($row = $result->fetch_assoc()) {
-	    		$myArray[] = $row;
-			}
-			echo json_encode($myArray);
-			$response['result'] = 'ok';
-			$response['code'] = 200;
-			$response['message'] = json_encode($myArray);
-		}
-		// chiudo la connessione
-		mysqli_close($link);
-	}
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
+///////////
+$accessToken = null;
+$loggedUsername = null;
+$loggedRole = null;
+//
+
+if(isset($_GET['accessToken'])) {
+        $accessToken = $_GET['accessToken'];
+        //echo($oicd_address);
+        $oidc = new OpenIDConnectClient($ssoEndpoint, $ssoClientId, $ssoClientSecret);
+
+        $oidc->setVerifyHost(false);
+        $oidc->setVerifyPeer(false);
+
+        $oidc->setAccessToken($accessToken);
+
+        $oidc->providerConfigParam(array('authorization_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/auth'));
+        $oidc->providerConfigParam(array('token_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/token'));
+        $oidc->providerConfigParam(array('userinfo_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/userinfo'));
+        $oidc->providerConfigParam(array('jwks_uri'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/certs'));
+        $oidc->providerConfigParam(array('issuer'=>$oicd_address.'/auth/realms/master'));
+        $oidc->providerConfigParam(array('end_session_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/logout'));
+
+        //$oidc->addScope(array('openid','username','profile'));
+    //$oidc->setRedirectURL($appUrl . '/ssoLogin.php');
+        try {
+                $requestUserInfo = $oidc->requestUserInfo("preferred_username");
+                if($requestUserInfo == null){
+                    $requestUserInfo = $oidc->requestUserInfo("username");
+                }
+                $requestUserInfo_string= json_encode($requestUserInfo);
+                //var_dump($requestUserInfo);
+                if($requestUserInfo  !== null){
+///////////////////
+                        $suri = $_GET["suri"]; //mi prendo il service uri come parametro dalla dall'uri
+                        $response = []; //inizializzo la response da ritornare quando faccio la chiamata
+                        $myArray = array(); //inizializzo se trovo valori
+                        //mi connetto al db
+                        $link =  mysqli_connect($host, $username, $password);
+                        mysqli_select_db($link, "processloader_db");
+
+                        // se la connessione non è andata a buon fine
+                        if($link == false){
+                                $response['result'] = 'KO';
+                                $response['code'] = 500;
+                                $response['message'] = 'DB Connection error';
+                                mysqli_close($link); // chiudo la connessione
+                                echo json_encode($response); //mando il messaggio di errore
+                        }else{
+                                //faccio la query
+                                $dateObserved = '';
+                                if(isset($_GET['dateObserved'])) {
+                                        if (($_GET['dateObserved'] != null)&&($_GET['dateObserved'] !== '')){
+                                                $dateObserved = $_GET['dateObserved'];
+                                        }
+
+                                }
+
+                                if($dateObserved !=''){
+                                        $sql = "SELECT * FROM processloader_db.bigdatafordevice where suri= '$suri' and dateObserved = '$dateObserved' order                                                                          by dateObserved desc ";
+                                }else{
+                                        $sql = "SELECT * FROM processloader_db.bigdatafordevice where suri= '$suri' order by dateObserved desc ";
+                                }
+
+                                $result = mysqli_query($link, $sql);
+                                if(false === $result) { // se non va a buon fine
+                                        echo "Errore: impossibile eseguire la query. " . mysqli_error($conn);
+                                        $response['result'] = 'KO';
+                                        $response['code'] = 500;
+                                        $response['message'] = 'DB query error';
+                                }else{ //altrimenti metto tutto in myArray e lo restituisco come json encoded
+                                        while($row = $result->fetch_assoc()) {
+                                                $myArray[] = $row;
+                                        }
+                                        echo json_encode($myArray);
+                                        $response['result'] = 'ok';
+                                        $response['code'] = 200;
+                                        $response['message'] = json_encode($myArray);
+                                        $response['userInfo'] = $requestUserInfo;
+                                }
+                                // chiudo la connessione
+                                mysqli_close($link);
+                        }
+            }else{
+                    $response['result'] = 'KO';
+                    $response['code'] = 401;
+                    $response['message'] = 'Not found user: '.$requestUserInfo; // chiudo la connessione
+                    echo json_encode($response);
+            }
+
+} catch(Exception $ex) {
+                $response['result'] = 'KO';
+                $response['code'] = 401;
+                $response['message'] = 'Not authorized'; // chiudo la connessione
+        echo json_encode($response); //mando il messaggio di errore
+  }
+
+
+}else{
+        $response['result'] = 'KO';
+                $response['code'] = 401;
+                $response['message'] = 'Not authorized'; // chiudo la connessione
+        echo json_encode($response); //mando il messaggio di errore
+}
 ?>
-
--- www/api/bigdatafordevice/postIt.php
+++ www/api/bigdatafordevice/postIt.php
@@ -2,13 +2,43 @@
 	header("Access-Control-Allow-Origin: *\r\n");
 	include('../../config.php');  // mi prendo i valori per la connessione: host username password 
 	include('../../curl.php');
+	require ('../../sso/autoload.php');
+	use Jumbojett\OpenIDConnectClient;	
+
+$accessToken = null;
+$loggedUsername = null;
+$loggedRole = null;
+//
+
+if(isset($_REQUEST['accessToken'])) {
+	//echo($_GET['accessToken']);
+	$accessToken = $_GET['accessToken'];
+	//echo($oicd_address);
+	$oidc = new OpenIDConnectClient($ssoEndpoint, $ssoClientId, $ssoClientSecret);
+
+	$oidc->setVerifyHost(false);
+	$oidc->setVerifyPeer(false);
+	$oidc->setAccessToken($accessToken);
+
+	$oidc->providerConfigParam(array('authorization_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/auth'));
+	$oidc->providerConfigParam(array('token_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/token'));
+	$oidc->providerConfigParam(array('userinfo_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/userinfo'));
+	$oidc->providerConfigParam(array('jwks_uri'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/certs'));
+	$oidc->providerConfigParam(array('issuer'=>$oicd_address.'/auth/realms/master'));
+	$oidc->providerConfigParam(array('end_session_endpoint'=>$oicd_address.'/auth/realms/master/protocol/openid-connect/logout'));
+
+	//$oidc->addScope(array('openid','username','profile'));
+    //$oidc->setRedirectURL($appUrl . '/ssoLogin.php');
+	try {
+		$requestUserInfo = $oidc->requestUserInfo("preferred_username");
+		if($requestUserInfo  !== null){
 
 	$suri = $_GET["suri"]; //mi prendo il service uri come parametro dalla dall'uri
 	$data = file_get_contents('php://input'); //e anche i dati nel body
 	$encoded_data = json_encode($data); 
 	$response = []; //inizializzo la response da ritornare quando faccio la chiamata
 
-	// Controllo se il parametro URI è presente
+	// Controllo se il parametro URI � presente
 	if ($suri === null || $suri === "") {
 		$response['result'] = 'KO';
 		$response['code'] = 400;
@@ -17,7 +47,7 @@
 		exit();
 	}else{
 		// Controllo se i dati nel body non sono vuoti
-		if (empty($encoded_data) || $encoded_data === "") {
+		if (empty($encoded_data) || $encoded_data === "" || $encoded_data === null ) {
 			$response['result'] = 'KO';
 			$response['code'] = 400;
 			$response['message'] = 'Empty request data';
@@ -28,7 +58,7 @@
 			$link =  mysqli_connect($host, $username, $password);
 			mysqli_select_db($link, "processloader_db");
 
-			// se la connessione non è andata a buon fine
+			// se la connessione non � andata a buon fine
 			if($link == false){
 				$response['result'] = 'KO';
 				$response['code'] = 500;
@@ -39,28 +69,85 @@
 		    }else{
 		    	// Escape dei dati per evitare SQL injection
 				$suri = mysqli_real_escape_string($link, $suri);
-				//$encoded_data = mysqli_real_escape_string($link, $encoded_data);    	
-
+				//$encoded_data = mysqli_real_escape_string($link, $encoded_data); 
+				$dateObserved = '';
+					if(isset($_REQUEST['dateObserved'])) {
+						if (($_REQUEST['dateObserved'] != null)&&($_REQUEST['dateObserved'] !== '')){
+							$dateObserved = $_REQUEST['dateObserved'];
+						}
+						
+					}   	
 		    	//faccio la query
-				$sql = "INSERT INTO processloader_db.bigdatafordevice (`suri`, `dateObserved`, `data`) VALUES ('$suri', now(), $encoded_data);";
-				$result = mysqli_query($link, $sql);
-				if(false === $result) { // se non va a buon fine la query
-			    	echo "Errore: impossibile eseguire la query. " . mysqli_error($conn);
-			    	$response['result'] = 'KO';
-					$response['code'] = 500;
-					$response['message'] = 'DB query error';
-				}else{ //altrimenti tutto ok
-					$response['result'] = 'ok';
-					$response['code'] = 200;
-					$response['message'] = "ok query andata";    	
-				}	
+				
+				if (isJsonWithElement($data, "grandidati")) {
+					//echo "L'elemento 'grandidati' esiste nel JSON.";
+					if($dateObserved !=''){
+						$sql = "INSERT INTO processloader_db.bigdatafordevice (`suri`, `dateObserved`, `data`) VALUES ('$suri', '$dateObserved', $encoded_data);";
+					}else{
+						$sql = "INSERT INTO processloader_db.bigdatafordevice (`suri`, `dateObserved`, `data`) VALUES ('$suri', now(), $encoded_data);";
+					}
+					////
+						$result = mysqli_query($link, $sql);
+						if(false === $result) { // se non va a buon fine la query
+							//echo "Errore: impossibile eseguire la query. " . mysqli_error($conn);
+							$response['result'] = 'KO';
+							$response['code'] = 500;
+							$response['message'] = 'DB query error';
+						}else{ //altrimenti tutto ok
+							$response['result'] = 'ok';
+							$response['code'] = 200;
+							$response['message'] = "ok query executed";    	
+						}	
+						mysqli_close($link);
+						echo json_encode($response); 
+						exit();
+			} else {
+				//echo "L'elemento 'grandidati' non esiste nel JSON o il JSON non è valido.";
+				//echo($encoded_data);
+				$response['result'] = 'ok';
+				$response['code'] = 500;
+				$response['message'] = "Not correct data";
 				mysqli_close($link);
-				exit();	
+				exit();	 
+				///////////
+			}
+				
 		    }
+		
 
 		}
 	}
 
-	
-	    
+}else{
+	$response['result'] = 'KO';
+				$response['code'] = 401;
+				$response['message'] = 'Not found user: '.$requestUserInfo; // chiudo la connessione
+    			echo json_encode($response);
+}
+
+} catch(Exception $ex) {
+	$response['result'] = 'Error';
+	$response['code'] = 404;
+	$response['message'] = $ex; // chiudo la connessione
+	echo json_encode($response); //mando il messaggio di errore
+}
+
+}else{
+	$response['result'] = 'KO';
+		$response['code'] = 401;
+		$response['message'] = 'Not authorized'; // chiudo la connessione
+    	echo json_encode($response); //mando il messaggio di errore
+}
+
+function isJsonWithElement($encoded_data1, $element) {
+    // Decodifica il JSON
+    $decoded_data = json_decode($encoded_data1, true);
+    
+    // Controlla se la decodifica è riuscita e se l'elemento esiste
+    if (json_last_error() === JSON_ERROR_NONE && isset($decoded_data[$element])) {
+        return true;
+    } else {
+        return false;
+    }
+}    
 ?>
